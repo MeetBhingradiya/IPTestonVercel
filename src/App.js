0613fce8598d3cc113226cca29774e7f65b4f013
MEET BHINGRADIYA <102130001+MeetBhingradiya@users.noreply.github.com>
2023-04-23 21:14:13 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  //creating IP state
   const [ip, setIP] = useState("");
-
-  //creating function to load ip address from the API
-  // const getData = async () => {
-  //   const res = await axios.get('https://geolocation-db.com/json/')
-  //   console.log(res.data);
-  //   setIP(res.data.IPv4)
-  // }
-
-  // Updated Code
 
   const getData = async () => {
     const res = await axios.get("https://api.ipify.org/?format=json");
@@ -22,7 +12,6 @@ function App() {
   };
 
   useEffect(() => {
-    //passing getData method to the lifecycle method
     getData();
   }, []);
 
